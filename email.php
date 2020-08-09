b8f9dbc7e753cea841b7bb7e571d539ec25cc0c5
Marco <programacao.curso2016@gmail.com>
2020-08-08 21:16:03 -0300

    atualização

--- email.php
+++ email.php
@@ -1,5 +1,9 @@
 <?php 
 
+ if (isset($_POST[btnSend])) {
+
+    $msg = '';
+
         /**** Informação se o email está certo ******/
             if(isset($_POST['email']) && !empty($_POST['email']));
 
@@ -9,7 +13,7 @@
         $email = addslashes($_POST['email']);
         $Mensagem = addslashes($_POST['message']);
 
-        $to = "contato@tsacabamentoereforma.com.br";
+        $to = "tsacab28	@tsacabamentoereforma.com.br";
         $subject = "Contato - Ts Construtora";// corpo da mensagem
         $body = "Nome: ".$nome. "\r\n".
                 "Telefone: ".$telefone. "\r\n".
@@ -22,12 +26,34 @@
                     
         if(mail($to,$subject,$body,$heade)) {
 
+            //mostra mensagem de envio com sucesso
+            $msg = '<div class="alert alert-success alert-dismissable">
+                        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                        <strong>Mensagem enviada com sucesso!</strong> 
+                    </div>';
+
             echo("Email enviado com sucesso!");
 
         }else{
 
+             //mostra mensagem de erro ao enviar
+             $msg = '<div class="alert alert-danger alert-dismissable">
+             <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+             <strong>Erro ao enviar mensagem, tente novamente! </strong> 
+         </div>';
+     
+
             echo("Email não pode ser enviado");
             
+        } else {
+        
+            //mostra mensagem de erro caso algum dos campos esteja vazio
+            $msg = '<div class="alert alert-danger alert-dismissable">
+                    <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+                    <strong>Preencha todos os campos!! </strong> 
+                </div>';
         }
+    
+    }     
 
 ?>
